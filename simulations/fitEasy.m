6d248d79902d340589fa7fdb3ec68eabd4c8322b
MPR Lab <mprlab@MPRs-Mac-Pro-2.local>
2017-09-26 15:16:30 -0400

    Redid fitting for wg_v5/rela1.

--- simulations/fitEasy.m
+++ simulations/fitEasy.m
@@ -6,7 +6,7 @@ numStarts = 10;
 numSubjects = 258;
 numFnEvals = 200;
 
-priorPDFs = {@(x) log(1/3), @(x) log(gampdf(x, 4.5, 1)), @(x), @(x) log(gampdf(x, 4.5, 1)), ...
+priorPDFs = {@(x) log(1/3), @(x) log(gampdf(x, 4.5, 1)), @(x) log(gampdf(x, 4.5, 1)), ...
     @(x) log(unifpdf(x, 0, 1)), @(x) log(unifpdf(x, 0, 1))};
 
 main = ['fitting/' envName '/' simsName];
@@ -17,7 +17,7 @@ modelParams_all = {[1 -1 0 -1 -1], [1 -1 0 1 0], [1 -1 0 0 1], [1 0 0 0 0], ...
     [-1 -1 -1 -1 -1], [-1 -1 -1 1 0], [-1 -1 -1 0 1], [-1 0 -1 0 0], ...
     [-1 -1 -1 -1 -1], [-1 -1 -1 1 0], [-1 -1 .2 -1 -1], [-1 -1 .2 1 0]};
 
-whichModels = 5:8;
+whichModels = 1:8;
 
 modelNames = modelNames_all(whichModels);
 modelParams = modelParams_all(whichModels);
